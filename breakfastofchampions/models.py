39c67f5f1ef4024f5612205fa072f7a986116f40
realjessicabring <jessic4br1ng@gmail.com>
2025-05-31 18:24:00 -0400

    i tbh don't know but i should save it

--- breakfastofchampions/models.py
+++ breakfastofchampions/models.py
@@ -1,11 +1,14 @@
+from datetime import datetime, timezone, tzinfo
 from django.db import models
+import pytz
 
 class MarkdownContent(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     slug = models.SlugField(blank=True)
     image = models.ImageField(default='breakfastofchampions/static/hello.png', upload_to='assets/')
-    timestamp = models.DateField(auto_now=True)
+    eastern_tz = pytz.timezone('US/Eastern') 
+    timestamp = models.DateField(default=datetime.now(eastern_tz))
     intro = models.CharField(max_length=500, default="Nothing to see here!")
     class Meta:
         verbose_name_plural = "Markdown content"
--- breakfastofchampions/views.py
+++ breakfastofchampions/views.py
@@ -9,15 +9,15 @@ def index(request):
       return render(request, 'index.html')
 
 def blog(request):
-      markdown_content = MarkdownContent.objects.all()
+      markdown_content = MarkdownContent.objects.all().order_by('-timestamp')
       context = {"markdown_content": markdown_content}
       return render(request, 'blog.html', context)
 
-
 def markdown_content_view(request, slug):
+     print(request)
      markdown_content = get_object_or_404(MarkdownContent, slug=slug)
-     markdown_content = MarkdownContent.objects.first()
      context = {"markdown_content": markdown_content}
+# print(markdown_content.title);
      return render(
         request,
         "breakfastofchampions/markdown_content.html",
